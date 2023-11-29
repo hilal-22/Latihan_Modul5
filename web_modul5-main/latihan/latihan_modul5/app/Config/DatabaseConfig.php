f3382432bf93ab88b06a6c09e65996e060876b19
hilal-22 <127408185+hilal-22@users.noreply.github.com>
2023-11-29 15:35:26 +0700

    Add files via upload

---
+++ web_modul5-main/latihan/latihan_modul5/app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\config;
+
+class DatabaseConfig {
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "prak_modul5";
+    public $port = 3306;
+}
